0158444e6a9b3f1fc83b4faee2191c9cdbe6c547
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-02 12:09:05 +0200

    Logout user

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ export default function setupRoutes(app: Application) {
   const routes = () => {
     app.use('/queues', serverAdapter.getRouter());
     app.use(BASE_PATH, authRoutes.routes());
+    app.use(BASE_PATH, authRoutes.signOutRoute());
   };
 
   routes();
