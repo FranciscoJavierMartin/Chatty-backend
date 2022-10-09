6b58236779b012cf6bce07db48213046b4999b7b
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-09 08:25:59 +0200

    Send email

--- src/shared/services/queues/email.queue.ts
+++ src/shared/services/queues/email.queue.ts
@@ -1,9 +1,11 @@
 import { BaseQueue } from '@service/queues/base.queue';
 import { EmailJob } from '@user/interfaces/user.interface';
+import { emailWorker } from '@worker/email.worker';
 
 class EmailQueue extends BaseQueue {
   constructor() {
     super('emails');
+    this.processJob('forgotPasswordEmail', 5, emailWorker.addNotificationEmail);
   }
 
   public addEmailJob(name: string, data: EmailJob): void {
