9e2a25637fa1b636d59303a87369ffd2734361f6
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-10 22:30:36 +0200

    Remove imports

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -7,16 +7,8 @@ import { loginSchema } from '@auth/schemes/signin';
 import { AuthDocument } from '@auth/interfaces/auth.interface';
 import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
-import {
-  ResetPasswordParams,
-  UserDocument,
-} from '@user/interfaces/user.interface';
+import { UserDocument } from '@user/interfaces/user.interface';
 import { userService } from '@service/db/user.service';
-import { forgotPasswordTemplate } from '@service/emails/templates/forgot-password/forgot-password-template';
-import { emailQueue } from '@service/queues/email.queue';
-import moment from 'moment';
-import publicIP from 'ip';
-import { resetPasswordTemplate } from '@service/emails/templates/reset-password/reset-password-template';
 
 export class SignIn {
   @joiValidation(loginSchema)
