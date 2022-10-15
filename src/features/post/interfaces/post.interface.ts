440d20095c30409d7a9274d3aee59b576bc720c1
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-15 12:49:27 +0200

    Get posts from cache

--- src/features/post/interfaces/post.interface.ts
+++ src/features/post/interfaces/post.interface.ts
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb';
 import mongoose, { Document } from 'mongoose';
 
-interface Reactions {
+export interface Reactions {
   like: number;
   love: number;
   happy: number;
--- src/shared/services/redis/post.cache.ts
+++ src/shared/services/redis/post.cache.ts
@@ -1,6 +1,20 @@
 import { ServerError } from '@global/helpers/error-handler';
-import { SavePostToCache } from '@post/interfaces/post.interface';
+import { Helpers } from '@global/helpers/helpers';
+import {
+  PostDocument,
+  Reactions,
+  SavePostToCache,
+} from '@post/interfaces/post.interface';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
 import { BaseCache } from '@service/redis/base.cache';
+
+export type PostCacheReturnedType =
+  | string
+  | number
+  | Buffer
+  | RedisCommandRawReply[]
+  | PostDocument
+  | PostDocument[];
 
 export class PostCache extends BaseCache {
   constructor() {
@@ -85,6 +99,47 @@ export class PostCache extends BaseCache {
       const count: number = parseInt(postCount[0], 10) + 1;
       multi.HSET(`users:${currentUserId}`, ['postsCount', count]);
       multi.exec();
+    } catch (error) {
+      this.log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+
+  public async getPostsFromCache(
+    key: string,
+    start: number,
+    end: string
+  ): Promise<PostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, start, end, {
+        REV: true,
+      });
+
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+
+      for (const value of reply) {
+        multi.HGETALL(`posts:${value}`);
+      }
+
+      const replies: PostCacheReturnedType = await multi.exec();
+      const postReplies: PostDocument[] = [];
+
+      for (const post of replies as unknown[] as PostDocument[]) {
+        if (post) {
+          post.commentsCount = Helpers.parseJson(
+            `${post.commentsCount}`
+          ) as number;
+          post.reactions = Helpers.parseJson(`${post.reactions}`) as Reactions;
+          post.createdAt = new Date(Helpers.parseJson(`${post.createdAt}`));
+          postReplies.push(post);
+        }
+      }
+
+      return postReplies;
     } catch (error) {
       this.log.error(error);
       throw new ServerError('Server error. Try again');
