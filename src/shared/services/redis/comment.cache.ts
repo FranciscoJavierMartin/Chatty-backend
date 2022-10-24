14b4535151389ac336f16a538570a551f6e0a91e
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-24 21:58:35 +0200

    Get comments names from cache

--- src/shared/services/redis/comment.cache.ts
+++ src/shared/services/redis/comment.cache.ts
@@ -1,4 +1,7 @@
-import { CommentDocument } from '@comment/interfaces/comment.interface';
+import {
+  CommentDocument,
+  CommentNameList,
+} from '@comment/interfaces/comment.interface';
 import { ServerError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 import { BaseCache } from '@service/redis/base.cache';
@@ -52,6 +55,41 @@ export class CommentCache extends BaseCache {
       }
 
       return list;
+    } catch (error) {
+      this.log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+
+  public async getCommentsNamesFromCache(
+    postId: string
+  ): Promise<CommentNameList[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const commentsCount: number = await this.client.LLEN(
+        `comments:${postId}`
+      );
+      const comments: string[] = await this.client.LRANGE(
+        `comments:${postId}`,
+        0,
+        -1
+      );
+      const list: string[] = [];
+
+      for (const item of comments) {
+        const comment: CommentDocument = Helpers.parseJson(item);
+        list.push(comment.username);
+      }
+
+      return [
+        {
+          count: commentsCount,
+          names: list,
+        },
+      ];
     } catch (error) {
       this.log.error(error);
       throw new ServerError('Server error. Try again');
