48f5885ff8ac4b7b08c656310964c952dbde4d8e
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 07:48:21 +0100

    Block user in DB

---
+++ src/shared/services/db/block-user.service.ts
@@ -0,0 +1,33 @@
+import { UserModel } from '@user/models/user.schema';
+import mongoose from 'mongoose';
+
+class BlockUserService {
+  public async blockUser(userId: string, followerId: string): Promise<void> {
+    await Promise.all([
+      UserModel.findOneAndUpdate(
+        {
+          _id: userId,
+          blocked: { $ne: new mongoose.Types.ObjectId(followerId) },
+        },
+        {
+          $push: {
+            blocked: new mongoose.Types.ObjectId(followerId),
+          },
+        }
+      ),
+      UserModel.findOneAndUpdate(
+        {
+          _id: followerId,
+          blockedBy: { $ne: new mongoose.Types.ObjectId(userId) },
+        },
+        {
+          $push: {
+            blockedBy: new mongoose.Types.ObjectId(userId),
+          },
+        }
+      ),
+    ]);
+  }
+}
+
+export const blockUserService: BlockUserService = new BlockUserService();
