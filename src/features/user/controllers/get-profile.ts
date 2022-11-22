999749ba055624cbcf16e4a2da64642279da6467
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-22 19:17:26 +0100

    Get profiles

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -49,6 +49,35 @@ export class Get {
     });
   }
 
+  public async profile(req: Request, res: Response): Promise<void> {
+    const cachedUser: UserDocument | null = await userCache.getUserFromCache(
+      req.currentUser!.userId
+    );
+
+    const existingUser: UserDocument = cachedUser
+      ? cachedUser
+      : await userService.getUserById(req.currentUser!.userId);
+
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ message: 'Get user profile', user: existingUser });
+  }
+
+  public async profileByUserId(req: Request, res: Response): Promise<void> {
+    const { userId } = req.params;
+    const cachedUser: UserDocument | null = await userCache.getUserFromCache(
+      userId
+    );
+
+    const existingUser: UserDocument = cachedUser
+      ? cachedUser
+      : await userService.getUserById(userId);
+
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ message: 'Get user profile by id', user: existingUser });
+  }
+
   private async allUsers({
     newSkip,
     limit,
