f5ca830366a1579ed6ec601963b935fd2fc7e556
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-23 20:38:54 +0100

    User update in queue

--- src/shared/services/queues/user.queue.ts
+++ src/shared/services/queues/user.queue.ts
@@ -1,14 +1,16 @@
 import { BaseQueue } from '@service/queues/base.queue';
+import { UserJob } from '@user/interfaces/user.interface';
 import { userWorker } from '@worker/user.worker';
 
 class UserQueue extends BaseQueue {
   constructor() {
     super('user');
     this.processJob('addUserToDB', 5, userWorker.addUserToDB);
+    this.processJob('updateUserInfo', 5, userWorker.updateUserInfo);
+    this.processJob('updateSocialLinks', 5, userWorker.updateSocialLinks);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public addUserJob(name: string, data: any): void {
+  public addUserJob(name: string, data: UserJob): void {
     this.addJob(name, data);
   }
 }
